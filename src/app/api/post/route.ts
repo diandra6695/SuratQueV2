626d0982dbfe05a389b3632718b631c2d95bf109
diandra6695 <andracom108@gmail.com>
2024-03-28 22:42:06 +0700

    feat: add upload file

--- src/app/api/post/route.ts
+++ src/app/api/post/route.ts
@@ -1,4 +1,5 @@
 import prisma from "@/lib/db";
+import { writeFile } from "fs/promises";
 import { NextRequest, NextResponse } from "next/server";
 
 export const GET = async (req: NextRequest) => {
@@ -6,15 +7,29 @@ export const GET = async (req: NextRequest) => {
   return NextResponse.json({ posts });
 };
 
-export const POST = async (req: NextRequest) => {
-  const { title, content } = await req.json();
+export const POST = async (req: NextRequest): Promise<Response> => {
+  const { title, content, file } = await req.json();
+
+  // const file: File | null = data.get("file") as unknown as File;
+
+  // if (!file) {
+  //   return NextResponse.json({ success: false });
+  // }
+
+  // const bytes = await file.arrayBuffer();
+  // const buffer = Buffer.from(bytes);
+
+  // // With the file data in the buffer, you can do whatever you want with it.
+  // // For this, we'll just write it to the filesystem in a new location
+  // const path = `/uploads/${file.name}`;
+  // await writeFile(path, buffer);
 
   const post = await prisma.post.create({
     data: {
       title,
       content,
+      file,
     },
   });
-
   return NextResponse.json({ post });
 };
